d4bcce5c987c283e1c9119a9d06d160da0167437
Trinh Duc Duy <duytd.hanu@gmail.com>
2015-09-04 08:50:30 +0700

    Finish dashboard layout

--- app/assets/javascripts/merchant/application.js
+++ app/assets/javascripts/merchant/application.js
@@ -2,4 +2,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require jquery.nicescroll/jquery.nicescroll
 //= require_tree .
---
+++ app/assets/javascripts/merchant/base.js
@@ -0,0 +1,17 @@
+$(document).on("page:change", function() {
+  $sideBar = $("aside.left-panel");
+  $content = $("section.content");
+  $sideBar.niceScroll({
+    cursorcolor: "#5c6d7e",
+    background: "#34495e",
+    cursorborder: "none"
+  });
+  $(".navbar-toggle").click(function(){
+    $sideBar.toggleClass("collapsed");
+    $content.toggleClass("expansed");
+    $sideBar.find(".subitems, .item-label, .merchant-logo .name").toggleClass("hide");
+  });
+  $sideBar.on("click", ".item", function(){
+    $(this).find(".subitems").slideToggle();
+  })
+});
