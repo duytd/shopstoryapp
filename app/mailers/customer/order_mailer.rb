2f3b17b1d4be5196e5062e04e84375ac9ced0839
Trinh Duc Duy <duytd.hanu@gmail.com>
2016-04-20 10:16:21 +0700

    Update mailer layout, change order cookies name

--- app/mailers/customer/order_mailer.rb
+++ app/mailers/customer/order_mailer.rb
@@ -1,4 +1,8 @@
 class Customer::OrderMailer < ApplicationMailer
-  def confirmation
+  def confirmation customer, locale
+    @template = EmailTemplate.find_by_name "confirmation.#{locale}.liquid"
+    @props = {
+      'customer'=>customer
+    }
   end
 end
