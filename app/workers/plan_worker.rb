44c37e907e54f2aaade715ee298d1d1f80679ef2
Duy Trinh Duc <duy@ubercarshare.com>
2024-06-28 23:14:32 +0700

    Remove sidekiq

--- app/workers/plan_worker.rb
+++ app/workers/plan_worker.rb
@@ -1,6 +1,6 @@
 class PlanWorker
-  include Sidekiq::Worker
-  sidekiq_options retry: 5, dead: false
+  # include Sidekiq::Worker
+  # sidekiq_options retry: 5, dead: false
 
   def perform stripe_id, action
     Plan.send "#{action}_stripe_plan", stripe_id
