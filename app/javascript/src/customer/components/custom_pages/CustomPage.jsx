ba108a22b8b65a6287d66b0f690266565ab17308
Nhims <nhims@Nhimss-MacBook-Pro.local>
2024-10-12 11:31:53 +0700

    Add globalVars state

--- app/javascript/src/customer/components/custom_pages/CustomPage.jsx
+++ app/javascript/src/customer/components/custom_pages/CustomPage.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import withCartMixins from '../../mixins/CartMixin';
 
 class CustomPageComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      globalVars: this.props.globalVars,
+    };
+  }
+
   render() {
     return CustomPageRT.apply(this);
   }
